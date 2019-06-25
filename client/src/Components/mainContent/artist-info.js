ba3d33fe4455658986ae7ea4314146c99bf3d1dd
ForsakenXD <41171766+ForsakenXD@users.noreply.github.com>
2019-06-25 05:24:11 +0300

    fixed the background gradient

--- client/src/Components/mainContent/artist-info.js
+++ client/src/Components/mainContent/artist-info.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Spotify from 'spotify-web-api-js'
 import {connect } from 'react-redux';
 import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+
 import { updateArtistId } from '../../store/actions/artist_id'
 
 class ArtistInfo extends Component {
@@ -11,7 +13,8 @@ class ArtistInfo extends Component {
     this.state = {
       nowPlaying:{
         name: 'Not Checked',
-        image: ''
+        image: '',
+        genres:[]
       }
     }
   }
@@ -29,28 +32,50 @@ class ArtistInfo extends Component {
         console.log('---------->',response.artists.items[0])
         this.setState({
           image:response.artists.items[0].images[0].url,
-          name:response.artists.items[0].name
+          name:response.artists.items[0].name,
+          genres:response.artists.items[0].genres
         })
 
         this.props.updateArtistId(response.artists.items[0].id)
-
+        console.log(this.state.genres)
       })
   }
 
 
   render(){
+  let listItems
   let content
+  console.log(this.state.genres)
+  // When genres render load them into the header
+  if(this.state.genres){
+     listItems = this.state.genres.map((genre,index) =>
+     <span className="header-description"  key={index}>{genre}</span>
+   );
+  }
 
   return (
 
       <header  alt={this.state.name} className="header-image" style={{backgroundImage: `url(${this.state.image})`}}>
         <h1 className="large" dir="auto">{this.state.name}</h1>
-        <button  className="btn" style={{color:'white',backgroundColor:'black'}}>
-          <div className="button-container" >
-                <h1>Check them out on</h1>
-                <img src="./soundcloud.png" alt="soundcloud" width="200" height="100" />
-          </div>
-        </button>
+        <Grid container direction="row" alignItems="center" justify="center">
+          {listItems}
+        </Grid>
+        <Box mb={5} spacing={2}>
+          <Grid container direction="row" alignItems='center' justify='center'>
+          <button  className="btn btn-sc" style={{color:'white',backgroundColor:'black'}}>
+            <div className="button-container" >
+                  <h1>Check them out on</h1>
+                  <img src="./soundcloud.png" alt="soundcloud" width="200" height="100" />
+            </div>
+          </button>
+          <button  className="btn btn-sp" style={{color:'white',backgroundColor:'black'}} onClick={() => {window.open(`https://open.spotify.com/artist/${this.props.artist_id}`)}}>
+            <div className="button-container" >
+                  <h1>Check them out on</h1>
+                  <img src="./spotify4.svg" alt="spotify" width="200" height="100" />
+            </div>
+          </button>
+          </Grid>
+        </Box>
       </header>
 
 )}
@@ -61,7 +86,8 @@ const mapStateToProps = (state) => {
   console.log('state:',state)
   return{
     token:state.userReducer.token,
-    search_result:state.userReducer.search_result
+    search_result:state.userReducer.search_result,
+    artist_id:state.userReducer.artist_id
   }
 }
 
